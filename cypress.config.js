32f76df02bd85765199ce2dc9c9d3660c25e5c53
Thimmaraju <thimmaraju418@gmail.com>
2024-01-03 08:19:22 +0530

    conditional, exec,session, excel

--- cypress.config.js
+++ cypress.config.js
@@ -6,7 +6,7 @@ module.exports = defineConfig({
   reporter: 'cypress-mochawesome-reporter',
   e2e: {
 
-    "chromeWebSecurity": false,
+   // "chromeWebSecurity": false,
     "viewportWidth": 1920,
     
     "viewportHeight": 1080,
---
+++ cypress/e2e/smoketests/cyexec.cy.js
@@ -0,0 +1,18 @@
+///<reference types="cypress"/>
+
+describe('Automation - Working cy.exec', function () {
+
+    it('Cypress Test Case - Understanding running commands in the tests ', function () {
+  
+      //exe file 
+     //cy.exec("C:/Users/Raju/Downloads/Postman-win64-Setup.exe", {failOnNonZeroExit: false})
+
+     // commands 
+    cy.exec("npx cypress open")
+
+     // run Js files 
+    //cy.exec(node sample.js)
+    
+    })
+
+  })
